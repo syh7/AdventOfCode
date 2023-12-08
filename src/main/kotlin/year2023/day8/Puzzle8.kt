2eb42c6bfbad222c1b62b152845367d0bdecea83
syh7 <sjoerd0707@live.nl>
2023-12-08 08:01:00 +0100

    Added year 2023 day 8

---
+++ src/main/kotlin/year2023/day8/Puzzle8.kt
@@ -0,0 +1,89 @@
+package year2023.day8
+
+import readFile
+
+data class Option(val name: String, val left: String, val right: String)
+
+fun main() {
+    val lines = readFile("year2023/day8/actual.txt")
+
+    val instructionOrderList = lines[0].split("").filter { it.isNotEmpty() }
+    println(instructionOrderList)
+
+    val map = readOptions(lines)
+    println(map)
+
+    val endValidationForA = { option: Option -> option.name == "ZZZ" }
+    val endValidationForB = { option: Option -> option.name.endsWith("Z") }
+
+    val stepsA = calculateSteps(map, instructionOrderList, map["AAA"]!!, endValidationForA)
+    println(stepsA)
+
+    val allStartsB = map.entries.filter { it.key.endsWith("A") }.map { it.value }
+    val allEndsB = allStartsB.map { calculateSteps(map, instructionOrderList, it, endValidationForB) }
+    println(allEndsB)
+    val lcm = calculateLCM(allEndsB.map { it.toLong() })
+    println(lcm)
+
+}
+
+private fun readOptions(lines: List<String>): MutableMap<String, Option> {
+    val map = mutableMapOf<String, Option>()
+    for (i in 2 until lines.size) {
+        println("Line: ${lines[i]}")
+        var (name, optionStr) = lines[i].split(" = ")
+        optionStr = optionStr.removePrefix("(")
+        optionStr = optionStr.removeSuffix(")")
+        val (left, right) = optionStr.split(", ")
+        map[name] = Option(name, left, right)
+    }
+    return map
+}
+
+private fun calculateSteps(
+    map: MutableMap<String, Option>,
+    instructionOrderList: List<String>,
+    start: Option,
+    expectedEnd: (Option) -> Boolean,
+): Int {
+    var steps = 0
+    var currentOption = start
+
+    while (!expectedEnd(currentOption)) {
+        for (instruction in instructionOrderList) {
+            currentOption = if (instruction == "L") {
+                map[currentOption.left]!!
+            } else {
+                map[currentOption.right]!!
+            }
+            steps++
+
+            if (expectedEnd(currentOption)) {
+                break
+            }
+        }
+    }
+    println("Option ${start.name} took $steps steps and ended in ${currentOption.name}")
+    return steps
+}
+
+private fun calculateLCM(numbers: List<Long>): Long {
+    var lcm = numbers[0]
+    for (i in 1 until numbers.size) {
+        lcm = calculateLCM(lcm, numbers[i])
+    }
+    return lcm
+}
+
+private fun calculateLCM(a: Long, b: Long): Long {
+    val largest = if (a > b) a else b
+    var lcm = largest
+    while (true) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            println("found lcm $lcm")
+            break
+        }
+        lcm += largest
+    }
+    return lcm
+}
