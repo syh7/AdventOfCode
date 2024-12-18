7ebbc52fd201b6bbe63cccc9cb42f804b49eebe1
syh7 <sjoerd0707@live.nl>
2024-12-18 14:31:14 +0100

    full names instead of shorthand

--- src/main/kotlin/syh/library/Direction.kt
+++ src/main/kotlin/syh/library/Direction.kt
@@ -15,17 +15,17 @@ data class Direction(val row: Int, val col: Int) {
     }
 
     companion object {
-        val N: Direction = Direction(-1, 0)
-        val E: Direction = Direction(0, 1)
-        val S: Direction = Direction(1, 0)
-        val W: Direction = Direction(0, -1)
-        val NE: Direction = Direction(-1, 1)
-        val SE: Direction = Direction(1, 1)
-        val SW: Direction = Direction(1, -1)
-        val NW: Direction = Direction(-1, -1)
+        val NORTH: Direction = Direction(-1, 0)
+        val EAST: Direction = Direction(0, 1)
+        val SOUTH: Direction = Direction(1, 0)
+        val WEST: Direction = Direction(0, -1)
+        val NORTH_EAST: Direction = Direction(-1, 1)
+        val SOUTH_EAST: Direction = Direction(1, 1)
+        val SOUTH_WEST: Direction = Direction(1, -1)
+        val NORTH_WEST: Direction = Direction(-1, -1)
 
-        val ALL_DIRECTIONS: List<Direction> = listOf(N, NE, E, SE, S, SW, W, NW)
-        val CARDINAL_DIRECTIONS: List<Direction> = listOf(N, E, S, W)
-        val ORDINAL_DIRECTIONS: List<Direction> = listOf(NE, SE, SW, NW)
+        val ALL_DIRECTIONS: List<Direction> = listOf(NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST)
+        val CARDINAL_DIRECTIONS: List<Direction> = listOf(NORTH, EAST, SOUTH, WEST)
+        val ORDINAL_DIRECTIONS: List<Direction> = listOf(NORTH_EAST, SOUTH_EAST, SOUTH_WEST, NORTH_WEST)
     }
 }
