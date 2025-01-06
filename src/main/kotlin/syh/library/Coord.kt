21c7725c334a82cc090c1c7f1f184670776cecaa
syh7 <sjoerd0707@live.nl>
2025-01-06 17:00:31 +0100

    override toString

--- src/main/kotlin/syh/library/Coord.kt
+++ src/main/kotlin/syh/library/Coord.kt
@@ -5,6 +5,9 @@ import kotlin.math.abs
 data class Coord(val row: Int, val column: Int) {
 
     fun toCoordString() = "[$row][$column]"
+    override fun toString(): String {
+        return toCoordString()
+    }
 
     fun relative(direction: Direction): Coord {
         return Coord(this.row + direction.row, this.column + direction.col)
