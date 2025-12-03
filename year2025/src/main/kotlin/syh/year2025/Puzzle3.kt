12885ca6018ab218d323688b6e7426ecb88b336c
syh7 <sjoerd0707@live.nl>
2025-12-03 06:41:50 +0100

    2025 day 3 v2

--- year2025/src/main/kotlin/syh/year2025/Puzzle3.kt
+++ year2025/src/main/kotlin/syh/year2025/Puzzle3.kt
@@ -7,23 +7,7 @@ class Puzzle3 : AbstractAocDay(2025, 3) {
         return readSingleLineFile(file)
             .sumOf { line ->
                 val numbers = line.split("").filter { it.isNotBlank() }.map { it.toInt() }
-                println(numbers)
-
-                var highest = 0
-                var secondHighest = 0
-
-                for (i in 9 downTo 1) {
-                    val index = numbers.indexOf(i)
-                    if (index == -1 || index == numbers.size - 1) {
-                        continue
-                    }
-                    highest = i
-                    secondHighest = numbers.drop(index + 1).max()
-                    break
-                }
-                val str = "$highest$secondHighest"
-                println(str)
-                str.toInt()
+                getHighestNumber(numbers, 2)
             }
             .toString()
     }
@@ -31,21 +15,25 @@ class Puzzle3 : AbstractAocDay(2025, 3) {
     override fun doB(file: String): String {
         return readSingleLineFile(file)
             .sumOf { line ->
-                var numbers = line.split("").filter { it.isNotBlank() }.map { it.toInt() }
-                println(numbers)
-
-                var total = ""
-
-                for (leftoverRequirement in 12 downTo 1) {
-                    val index = getHighestValidIndex(numbers, leftoverRequirement)
-                    total += numbers[index]
-                    numbers = numbers.drop(index + 1)
-
-                }
-                println(total)
-                total.toLong()
+                val numbers = line.split("").filter { it.isNotBlank() }.map { it.toInt() }
+                getHighestNumber(numbers, 12)
             }
             .toString()
+    }
+
+    private fun getHighestNumber(initial: List<Int>, totalSize: Int): Long {
+        var totalNumber = ""
+        var numbers = initial
+        for (leftoverRequirement in totalSize downTo 1) {
+            val index = getHighestValidIndex(numbers, leftoverRequirement)
+            totalNumber += numbers[index]
+            numbers = numbers.drop(index + 1)
+
+        }
+        println(initial)
+        println(totalNumber)
+        return totalNumber.toLong()
+
     }
 
     private fun getHighestValidIndex(numbers: List<Int>, leftoverRequirement: Int): Int {
